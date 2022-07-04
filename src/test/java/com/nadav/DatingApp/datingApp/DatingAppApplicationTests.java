662430b1636dd082c36b6dccd10d82f7e6b2d610
iamSherez <iam_sherez@hotmail.com>
2022-07-04 20:21:01 +0300

    First Commit

---
+++ src/test/java/com/nadav/DatingApp/datingApp/DatingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nadav.DatingApp.datingApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
